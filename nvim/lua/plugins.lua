d3bd1e368d8285bb6d18c1eac450d0e0219e86d0
Aleksandr Bubnov <69006636+qwexter@users.noreply.github.com>
2025-11-10 16:21:27 +0400

    Use plugins from nvim.mini

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,4 +1,14 @@
 vim.pack.add({
+	-- theme
   { src = "https://github.com/Shatur/neovim-ayu" },
+	-- lsp
   { src = 'https://github.com/neovim/nvim-lspconfig' },
+	-- surrounding by a hotkey	
+	{ src = "https://github.com/nvim-mini/mini.surround" },
+	-- same bahavior with inserting closing brackes etc, as in Intellij
+	{ src = "https://github.com/nvim-mini/mini.pairs" },
 })
+
+
+require('mini.pairs').setup()
+require('mini.surround').setup()
